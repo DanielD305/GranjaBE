c6dee8cd0d5b43d264a2c0dd1c034fb1db1c8858
Walfred Donis <walfreddonis1@gmail.com>
2024-02-20 11:57:47 -0600

    Primer Commit

---
+++ Controllers/InformacionController.cs
@@ -0,0 +1,48 @@
+﻿using GenesisEfectivo_BE.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Granja.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformacionController : ControllerBase
+    {
+        private readonly ResponseMessage response = new();
+
+        [HttpGet("[action]")]
+        public IActionResult GetInformacion()
+        {
+            try
+            {
+
+
+                List<String> Informacion = new();
+
+
+                for (int i = 0; i < 8; i++ )
+                {
+                    Informacion.Add(i.ToString());
+                }
+
+
+
+                response.Code = "200";
+                response.Response = "OK";
+                response.data = Informacion;
+
+                return StatusCode(200, response);
+            }
+            catch (Exception e)
+            {
+                response.Code = "500";
+                response.Response = "Error al obtener  la lista de informacion " + e.Message;
+
+                return StatusCode(500, response);
+            }
+        }
+
+
+
+    }
+}
---
+++ Utils/ResponseMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace GenesisEfectivo_BE.Utils
+{
+    public class ResponseMessage
+    {
+        public string Code { get; set; }
+        public string Response { get; set; }
+        public object data { get; set; } 
+
+        public object? extra { get; set; }
+    }
+}
+
